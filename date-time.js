5d8d72fc94f710b07d9b91b422fcf3598de7883b
Elevate JS <shihab2413@gmail.com>
2025-02-14 07:59:27 +0600

    Create date-time.js

---
+++ date-time.js
@@ -0,0 +1,17 @@
+const date = new Date();
+console.log(date);
+
+const date2 = new Date();
+console.log(date2.toLocaleDateString());
+
+const date3 = new Date();
+console.log(date3.toISOString());
+
+const date4 = new Date();
+console.log(date4.toDateString());
+
+const date5 = new Date();
+console.log(date5.toTimeString());
+
+const date6 = new Date();
+console.log(date6.toLocaleString());
