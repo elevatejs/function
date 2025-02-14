a27486de49fcd694fba8907e66ef4b124f6f34d9
Elevate JS <shihab2413@gmail.com>
2025-02-14 07:59:49 +0600

    Create math.js

---
+++ math.js
@@ -0,0 +1,24 @@
+let rand = Math.round(Math.random(1000) * 10000);
+console.log(rand);
+
+// max number 
+const number = Math.max(1, 5, 1, 5, 6, 545, 4554, 655, 85, 456);
+console.log(number);
+
+// min number 
+const number2 = Math.min(1, 5, 1, 5, 6, 545, 4554, 655, 85, 456);
+console.log(number2);
+
+const pi = Math.PI
+console.log(pi);
+
+
+const floor = Math.floor(4.9);
+console.log(floor);
+
+const ceiling = Math.ceil(4.9);
+console.log(ceiling);
+
+const absolute = Math.abs(10 - 20);
+console.log(absolute);
+
